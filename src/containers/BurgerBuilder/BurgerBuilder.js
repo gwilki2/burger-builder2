1487e5fa4f77ff6cbfac7ffe8c934a2b3305080c
George Wilkins <gwilkins@apprissretail.com>
2020-05-24 00:57:42 -0500

    initial changes

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+
+class BurgerBuilder extends React.Component{
+
+    render(){
+        return (
+            <Aux>
+                <p>Burger</p>
+                <p>Build Controls</p>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
