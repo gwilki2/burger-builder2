2e1e76f4bbba470853b28d698e10baa5eee51a25
George Wilkins <gwilkins@apprissretail.com>
2020-05-24 02:25:59 -0500

    added burger building from static state to prop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 
 class BurgerBuilder extends React.Component{
 
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2, 
+            meat: 2
+        }
+    }
+
     render(){
         return (
             <Aux>
-                <p>Burger</p>
-                <p>Build Controls</p>
+                <Burger ingredients={this.state.ingredients} />
+                <div>Build Controls</div>
             </Aux>
         );
     }
